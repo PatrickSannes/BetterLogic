31f7cb0454619b6670a50d7ec9b77cd166991fa1
Patrick Sannes <patrick@i-dev.net>
2016-02-24 22:13:33 +0100

    Add actions

--- net.i-dev.BetterLogic/lib/autocomplete/actions.js
+++ net.i-dev.BetterLogic/lib/autocomplete/actions.js
@@ -1,9 +1,6 @@
 ﻿var util = require('../util/util.js');
 var variableManager = require('../variablemanagement/variablemanagement.js');
 exports.createAutocompleteActions = function () {
-    //action autocomples
-    Homey.log(variableManager.getVariables());
-
     Homey.manager('flow').on('action.set_string_variable.variable.autocomplete', function (callback, value) {
         callback(null, variableManager.getVariables().filter(util.filterVariable(value, 'string')));
     });

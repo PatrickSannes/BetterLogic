4e260fe53d30e0c68332b00f23600f2b1fdc87b8
Patrick Sannes <patrick@i-dev.net>
2016-02-24 22:21:07 +0100

    Add value change logic

--- net.i-dev.BetterLogic/lib/flow/triggers.js
+++ net.i-dev.BetterLogic/lib/flow/triggers.js
@@ -1,8 +1,13 @@
 ﻿var variableManager = require('../variablemanagement/variablemanagement.js');
 exports.createTriggers = function () {
     Homey.manager('flow').on('trigger.if_variable_changed', function (callback, args, state) {
-        Homey.log(args);
-        callback(null, true); // true to make the flow continue, or false to abort
+        
+        var variable = variableManager.getVariables()[0]; //dirty hack. Last updated value is always on top..
+        if (args.variable.name == variable.name) {
+            callback(null, true); // true to make the flow continue, or false to abort
+            return;
+        }
+        callback(null, false); // true to make the flow continue, or false to abort
     });
 
     Homey.manager('flow').on('trigger.debug_any_variable_changed', function (callback, args, state) {
